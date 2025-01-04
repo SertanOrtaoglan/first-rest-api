4cb23ff8299f2206f29b8c24cb362a69d5a708d3
Sertan Ortaoğlan <sertanortaoglan@hotmail.com>
2025-01-05 01:29:21 +0300

    Creating a Hello World REST API with Spring Boot

---
+++ restful-web-services/src/main/java/com/in28minutes/rest/webservices/restful_web_services/helloworld/HelloWorldController.java
@@ -0,0 +1,22 @@
+package com.in28minutes.rest.webservices.restful_web_services.helloworld;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+//REST API
+@RestController
+public class HelloWorldController {
+	
+	//URL: /hello-world olacak ve "Hello World" yazısını ekrana yazdıracağız.
+	//@RequestMapping(method = RequestMethod.GET, path = "/hello-world")
+	@GetMapping(path = "/hello-world")
+	public String helloWorld() {
+		return "Hello World";
+	}
+	
+	
+	
+	
+	
+}
+
