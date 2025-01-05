cbd47b1406e8a97e9298b2ab9b9c2764eda956d4
Sertan Ortaoğlan <sertanortaoglan@hotmail.com>
2025-01-05 03:18:40 +0300

    Enhancing the Hello World REST API with a Path Variable

--- restful-web-services/src/main/java/com/in28minutes/rest/webservices/restful_web_services/helloworld/HelloWorldController.java
+++ restful-web-services/src/main/java/com/in28minutes/rest/webservices/restful_web_services/helloworld/HelloWorldController.java
@@ -1,6 +1,7 @@
 package com.in28minutes.rest.webservices.restful_web_services.helloworld;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 //REST API
@@ -19,6 +20,17 @@ public class HelloWorldController {
 	public HelloWorldBean helloWorldBean() {
 		return new HelloWorldBean("Hello World");
 	}
+	
+	
+	//Path Parameters  => URL'lerimizdeki değişken değerlere 'path parameters' denir.
+	//URL: /users/{id}/todos/{id}  ==> /users/1/todos/101  (Yani bu URL, belirli bir kullanıcının(1) belirli bir todo'suna(101) erişmek için kullanılır.) 
+	
+	//Şimdi yapacağımız örnek => URL: /hello-world/path-variable/{name} olacaktır. Örneğin "/hello-world/path-variable/Ranga" şeklinde istek atacağız. 
+	@GetMapping(path = "/hello-world/path-variable/{name}")
+	public HelloWorldBean helloWorldPathVariable(@PathVariable String name) {
+		return new HelloWorldBean(String.format("Hello World, %s", name));    //Burada yaptığımız şey; kullanıcı tarafından girilen String türündeki 'name'i almak ve bu method içerisinde yer alan '%s' ifadesini, girilen 'name' değeri ile değiştirmektir. Yani diyelim ki kullanıcı isteği "/hello-world/path-variable/Ranga" şeklinde yaptı.(Kullanıcı 'name' değerini path'ten 'Ranga' olarak göndermiş oldu.) Çıktı olarak kullanıcıya "messsage : Hello World, Ranga" ifadesini geri döndürürüz. 
+	}
+	
 	
 	
 	
